9578c24568dcb9afb9467f4a038a6db60a6e33d6
Cem Altun <hareketlicem@hotmail.com>
2018-03-23 00:46:05 +0100

    Attempt 2

--- Principal_Component_Analysis(PCA)/PCA.py
+++ Principal_Component_Analysis(PCA)/PCA.py
@@ -40,12 +40,12 @@ class PCA:
 	    
 	    
 	def fit(self, X, y=None):
-	    mean = np.array([np.mean(X.T, axis=0)])
-	    means = np.repeat(mean, X.shape[1], axis = 0)
+	    mean = np.array([np.mean(X, axis=0)])
+	    means = np.repeat(mean, X.shape[0], axis = 0)
 	    self.mean = mean
 	    
-	    X_ = X - means.T # Centered the Data
-	    C = np.dot(X_, X_.T)
+	    X_ = X - means # Centered the Data
+	    C = np.dot(X_.T, X_)
 	    [U, S, V] = LA.svd(C) # Apperantly C is not the Covariance matrix of X_
 	    
 	    self.covs = []
@@ -79,34 +79,53 @@ class PCA:
 	    
 	def transform(self, X):
 	    
-	    return np.dot(self.eigen_faces, X) 
+	    return np.dot(X, self.eigen_faces.T) 
 
 	def retransform(self, X):
-	    
-	    return np.dot(self.eigen_faces.T, X) +  np.repeat(self.mean, X.shape[1], axis = 0).T
+		
+	    return np.dot(X, self.eigen_faces)  +  np.repeat(self.mean, X.shape[0], axis = 0)
 
 
 if __name__ == '__main__':
 	X, y = read("mfeat-pix.txt")
 	digit = int(sys.argv[1]) # First, Enter Your digit
-	X_3 = X[digit*200:digit*200 + 200].T
+	X_ = X[digit*200:digit*200 + 200]
 
 	pca = PCA(float(sys.argv[2])) # Second, Enter Your percentage that you want to shrink
-	pca.fit(X_3)
-	X_pca = pca.transform(X_3)
+	pca.fit(X_)
+	X_pca = pca.transform(X_)
+	
 	X_constructed = pca.retransform(X_pca)
 
+	print("Number of eigen vectors used : ", pca.n_components)
+
+
 	# Plot some images
-	f, ax = plt.subplots(2,10, figsize=(16, 15))
-	for i in range(10):
-		ax[0,i].imshow(X_3[:,i].reshape(16,15), cmap="gray") 
-		ax[1,i].imshow(X_constructed[:,i].reshape(16,15), cmap="gray") 
+	f, ax = plt.subplots(2,5, figsize=(16, 15))
+	for i in range(5):
+		ax[0,i].imshow(X_[i,:].reshape(16,15), cmap="gray") 
+		ax[1,i].imshow(X_constructed[i,:].reshape(16,15), cmap="gray") 
 	f.suptitle("Reconstructed Images", fontsize=12)
 	plt.show()
 
 	# Plot top 5 eigen vectors Note if percentage of shrinking a lot it will not use top 5 eigen vectors
-	f2, ax2 = plt.subplots(1, 5, figsize=(16, 15))
-	for i in range(5):
-		ax2[i].imshow(pca.eigen_faces[i,:].reshape(16,15), cmap="gray") 
-	f2.suptitle("Top 5 eigen faces", fontsize=12)	
+	if pca.n_components < 5:
+		if pca.n_components != 1:
+			f2, ax2 = plt.subplots(1, pca.n_components, figsize=(16, 15))
+			for i in range(pca.n_components):
+				ax2[i].imshow(pca.eigen_faces[i,:].reshape(16,15), cmap="gray")
+		else:
+			f2, ax2 = plt.subplots(1, figsize=(16, 15))
+			ax2.imshow(pca.eigen_faces.reshape(16,15), cmap="gray")
+
+	else:
+		f2, ax2 = plt.subplots(1, 5, figsize=(16, 15))
+		for i in range(5):
+			ax2[i].imshow(pca.eigen_faces[i,:].reshape(16,15), cmap="gray")	
+	f2.suptitle("Top eigen vectors", fontsize=12)
+	plt.show()
+
+	plt.figure(3)
+	plt.plot(pca.covs)
+	plt.title("sum i to m (cov) / sum total (cov)")
 	plt.show()
